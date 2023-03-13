ddade11a855863ba8d41afbd88111d668e5e3af1
Yonwoo Choi <52492998+yc4ny@users.noreply.github.com>
2023-03-14 00:09:49 +0900

    Time: 43 ms (82.79%), Space: 14.5 MB (95.66%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        forward = ""
-        backward = ""
-        for i in range(len(s)):
-            if s[i].isalnum():
-                forward += s[i]
-            if s[len(s)-1-i].isalnum():
-                backward += s[len(s)-1-i]
-        if forward == backward:
+        temp = ""
+        for letter in s:
+            if letter.isalnum():
+                temp += letter
+        if temp == temp[::-1]:
             return True 
         else:
             return False
