0d2bc43eaf6ae11ce1d241b6c80aa87a07acaa3f
Yonwoo Choi <52492998+yc4ny@users.noreply.github.com>
2023-03-14 00:22:05 +0900

    Time: 46 ms (73.26%), Space: 14.9 MB (35.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
         s = "".join(x for x in s if x.isalnum()).lower()
         if s == s[::-1]:
             return True 
